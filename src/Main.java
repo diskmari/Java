8a22bc0657c9786f339702b6a8b60721953bf877
Maria Fernanda Resende <eumarimonteiro45140@gmail.com>
2024-09-18 16:18:39 -0300

    projeto

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maveric");
+    }
+
+}
--- src/SOMA.java
+++
@@ -1,5 +0,0 @@
-public class SOMA {
-    public static void main(String[] args) {
-        System.out.println(10 + 5);
-    }
-}
